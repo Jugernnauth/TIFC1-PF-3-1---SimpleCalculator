2992db59ac3be82ce5abf3547f936f2d3e35cb7d
Jugernnauth <fbustosphilomela@gmail.com>
2026-02-06 20:53:56 -0500

    Tarea realizada

---
+++ main.py
@@ -0,0 +1,9 @@
+
+#calculadora basica
+number_1= int(input ("ingresa el numero: "))
+number_2= int(input ("ingresa el numero: "))
+
+result = number_1 + number_2
+
+print ("El resultado es ")  
+input(result)
